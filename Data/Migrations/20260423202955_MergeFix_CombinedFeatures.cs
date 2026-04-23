033b3dcd74163d679e755d74699b8e8ac7303486
bethlehem-kebede <bethlehemkebede88@gmail.com>
2026-04-23 23:32:47 +0300

    Synchronize EF Core schema after branch merge

---
+++ Data/Migrations/20260423202955_MergeFix_CombinedFeatures.cs
@@ -0,0 +1,18 @@
+using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SCM_System.Data.Migrations
+{
+    public partial class MergeFix_CombinedFeatures : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+        }
+    }
+}
